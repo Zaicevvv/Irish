9726f1ff918206d4fc9ca84287a7cd0800291c22
ZaitsevVV <66888899+ZaitsevVV@users.noreply.github.com>
2021-02-23 13:52:06 +0200

    console log deleted

--- src/components/AvatarUpload.js
+++ src/components/AvatarUpload.js
@@ -1,35 +1,43 @@
-import React, { Component, useState } from 'react'
-import placeholder from '../assets/images/dest/ava.png'
-import config from '../config';
-
+import React, { Component, useState } from "react";
+import placeholder from "../assets/images/dest/ava.png";
+import config from "../config";
 
 const AvatarUpload = ({ onAvaSubmit, avatar }) => {
+  const [avaImg, setAvaImg] = useState(placeholder);
 
-    const [avaImg, setAvaImg] = useState(placeholder)
+  const changeHandler = (img) => {
+    onAvaSubmit(img);
+  };
 
-    const changeHandler = (img) => {
-        onAvaSubmit(img)
-        //console.log("changeHandler -> img", img) 
-    }
+  const deleteHandler = () => {
+    onAvaSubmit();
+  };
 
-    const deleteHandler = () => {
-        onAvaSubmit()
-    }
+  return (
+    <div className="avatart_wrapper row">
+      <div>
+        <img
+          src={avatar ? config.REACT_APP_ROOT + avatar : avaImg}
+          className="avatart_img"
+        />
+      </div>
+      <div>
+        <input
+          type="file"
+          id="avatar"
+          onChange={(e) => changeHandler(e.target.files)}
+          className="avatar_input"
+        />
+        <p className="upoload_descr">Your profile photo</p>
+        <label htmlFor="avatar" className="ava_change_label">
+          Change
+        </label>
+        <button className="ava_delete_label" onClick={deleteHandler}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className='avatart_wrapper row'>
-            <div>
-                <img src={avatar ? config.REACT_APP_ROOT + avatar : avaImg} className='avatart_img' />
-            </div>
-            <div>
-                <input type='file' id='avatar' onChange={(e) => changeHandler(e.target.files)} className='avatar_input' />
-                <p className='upoload_descr'>Your profile photo</p>
-                <label htmlFor='avatar' className='ava_change_label'>Change</label>
-                <button className='ava_delete_label' onClick={deleteHandler}>Delete</button>
-            </div>
-        </div>
-    )
-}
-
-
-export default AvatarUpload
+export default AvatarUpload;
