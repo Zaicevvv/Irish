ee5b80d7f0cb5b7ecfff08a1fc8701315d6fee00
ZaitsevVV <66888899+ZaitsevVV@users.noreply.github.com>
2021-02-23 13:49:28 +0200

    checkbox added

--- src/containers/Admin/CreateCourse/index.js
+++ src/containers/Admin/CreateCourse/index.js
@@ -1,49 +1,45 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import StaticPage from '../../../components/StaticPage'
-import headerData from '../../../constants/navData'
-import CreateCourseForm from './components/CreateCourseForm'
-import { createCategoryAction } from '../actions'
-import history from '../../../history'
-import _ from 'lodash'
-import { routeToCourseEdit } from '../../../constants/routes'
-
+import React from "react";
+import { connect } from "react-redux";
+import StaticPage from "../../../components/StaticPage";
+import headerData from "../../../constants/navData";
+import CreateCourseForm from "./components/CreateCourseForm";
+import { createCategoryAction } from "../actions";
+import history from "../../../history";
+import _ from "lodash";
+import { routeToCourseEdit } from "../../../constants/routes";
 
 const CreateCourse = ({ onSubmit, category, inProcess }) => {
-    
-    const onFormSubmit = async (values) => {
-        await onSubmit(values).then((res) => {
-            const courseId = _.get(res, 'action.payload.data.data.category.id')
-            if (res.value.status == 201 && courseId) {
-                history.push(routeToCourseEdit(courseId))
-            }
-        })
+  const onFormSubmit = async (values) => {
+    await onSubmit(values).then((res) => {
+      const courseId = _.get(res, "action.payload.data.data.category.id");
+      if (res.value.status == 201 && courseId) {
+        history.push(routeToCourseEdit(courseId));
+      }
+    });
+  };
 
-    }
-
-    return (
-        <StaticPage pageClass='create_course' headerData={headerData.admin}>
-            <div className='container'>
-                <CreateCourseForm onFormSubmit={onFormSubmit} />
-            </div>
-        </StaticPage>
-    )
-}
+  return (
+    <StaticPage pageClass="create_course" headerData={headerData.admin}>
+      <div className="container">
+        <CreateCourseForm onFormSubmit={onFormSubmit} />
+      </div>
+    </StaticPage>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
-    onSubmit: (data) => dispatch(createCategoryAction(data)),
-    //successNotification: (message) => dispatch(successNotificationAction(message))
-})
+  onSubmit: (data) => dispatch(createCategoryAction(data)),
+  //successNotification: (message) => dispatch(successNotificationAction(message))
+});
 
 const mapStateToProps = ({ admin }) => {
-    const data = {
-        category: admin.category,
-        inProcess: admin.inProcess
-    }
-    return data
-}
+  const data = {
+    category: admin.category,
+    inProcess: admin.inProcess,
+  };
+  return data;
+};
 
-CreateCourse.propTypes = {
-}
+CreateCourse.propTypes = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(CreateCourse)
+export default connect(mapStateToProps, mapDispatchToProps)(CreateCourse);
--- src/containers/Admin/EditCourse/components/EditCourseForm.js
+++ src/containers/Admin/EditCourse/components/EditCourseForm.js
@@ -1,167 +1,234 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { routeToLessonEdit } from '../../../../constants/routes'
-import LessonListItem from './LessonListItem'
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import placeholder from '../../../../assets/images/dest/ava.png'
-import config from '../../../../config';
-import _ from 'lodash'
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { routeToLessonEdit } from "../../../../constants/routes";
+import LessonListItem from "./LessonListItem";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import placeholder from "../../../../assets/images/dest/ava.png";
+import config from "../../../../config";
+import _ from "lodash";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
-const EditCourseForm = ({ onFormSubmit, handlerDelete, reqErrors, category, handlerLessonDelete, inProcess }) => {
-    const { register, handleSubmit, errors } = useForm();
+const EditCourseForm = ({
+  onFormSubmit,
+  handlerDelete,
+  reqErrors,
+  category,
+  handlerLessonDelete,
+  inProcess,
+}) => {
+  const { register, handleSubmit, errors } = useForm();
 
-    const [open, setOpen] = useState(false);
-    const [uploadedFileName, setUploadedFileName] = useState('+ Add image')
-    const [uploadedFile, setUploadedFile] = useState()
-    const [deleteFlag, setDeleteFlag] = useState()
+  const [open, setOpen] = useState(false);
+  const [uploadedFileName, setUploadedFileName] = useState("+ Add image");
+  const [uploadedFile, setUploadedFile] = useState();
+  const [deleteFlag, setDeleteFlag] = useState();
+  const [checked, setChecked] = useState(false);
 
-    const targetImage = category && category.icon ? category.icon.url : placeholder;
+  useEffect(() => {
+    setChecked(category.remote_learning || false);
+  }, []);
 
-    const onSubmit = data => {
-        let body = new FormData();
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
 
-        if (uploadedFile && typeof uploadedFile === 'object') {
-            body.append('[category][icon]', uploadedFile);
-        }
-        if (uploadedFile && uploadedFile == 'delete') {
-            body.append('[category][icon]', null);
-        }
-        _.mapKeys(data, (value, key) => {
-            if (key != 'course_img') {
-                body.append(`[category][${key}]`, value);
-            }
-        })
+  const targetImage =
+    category && category.icon ? category.icon.url : placeholder;
 
-        onFormSubmit(body)
-    };
+  const onSubmit = (data) => {
+    let body = new FormData();
 
-    const fileChangeHandler = (e) => {
-        if (e.target.files[0]) {
-            setUploadedFileName(e.target.files[0].name)
-            setUploadedFile(e.target.files[0])
-        }
+    if (uploadedFile && typeof uploadedFile === "object") {
+      body.append("[category][icon]", uploadedFile);
     }
-
-    const imageDeleteHandler = (e) => {
-        e.preventDefault()
-        setUploadedFileName('+ Add image')
-        setUploadedFile('delete')
-        setDeleteFlag(true)
+    if (uploadedFile && uploadedFile == "delete") {
+      body.append("[category][icon]", null);
     }
+    _.mapKeys(data, (value, key) => {
+      if (key != "course_img") {
+        body.append(`[category][${key}]`, value);
+      }
+    });
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+    body.append("[category][remote_learning]", checked);
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+    onFormSubmit(body);
+  };
 
-    return (
-        <div className='editCourseForm'>
+  const fileChangeHandler = (e) => {
+    if (e.target.files[0]) {
+      setUploadedFileName(e.target.files[0].name);
+      setUploadedFile(e.target.files[0]);
+    }
+  };
 
+  const imageDeleteHandler = (e) => {
+    e.preventDefault();
+    setUploadedFileName("+ Add image");
+    setUploadedFile("delete");
+    setDeleteFlag(true);
+  };
 
-            <form onSubmit={handleSubmit(onSubmit)} >
-                <div className='actionButtons'>
-                    <a className='alertColor' onClick={handleClickOpen}>DELETE COURSE</a>
-                    {!inProcess ? (
-                        <button disabled={inProcess} type="submit">SAVE CHANGES</button>
-                    ) : (
-                            <button disabled>LOADING...</button>
-                        )}
-                </div>
-                <h3 className='courseFormName'>Edit course</h3>
-                <div className='w-660'>
-                    <div className='mb-30 row'>
-                        <div className='placeholder_img'>
-                            <label htmlFor='course_img'>
-                                {targetImage && !deleteFlag ? (
-                                    <img src={config.REACT_APP_ROOT + targetImage} className='w-full' />
-                                ) : (
-                                        <p className='course_image_name'>{uploadedFileName}</p>
-                                    )}
-                            </label>
-                        </div>
-                        <div>
-                            <input onChange={fileChangeHandler} type='file' name='course_img' id='course_img' className='avatar_input' />
-                            {/* <p className='upoload_descr'>Add image</p> */}
-                            <label htmlFor='course_img' className='ava_change_label'>Change</label>
-                            <button className='ava_delete_label' onClick={imageDeleteHandler}>Delete</button>
-                        </div>
-                    </div>
-                    <p className='input_group_label'>Main info about course</p>
-                    <input
-                        className='input_dark w-full'
-                        name="title"
-                        ref={register({ required: true, max: 100 })}
-                        defaultValue={category.title}
-                        placeholder='Tittle (max 100 characters)'
-                    />
-                    {errors && errors.title && <span className='error'>Please, add valid title</span>}
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-                    <input
-                        className='input_dark w-full'
-                        name="description"
-                        ref={register({ required: true, max: 1000 })}
-                        defaultValue={category.description}
-                        placeholder='Description (max 1 000 characters)'
-                    />
-                    {errors && errors.description && <span className='error'>Please, add valid description</span>}
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-                    {reqErrors && reqErrors.title && reqErrors.title.length > 0 && reqErrors.title.map((error) => <span className='error'>{error}<br /></span>)}
-
-                    <p className='input_group_label'>List of the Module:</p>
-
-                    {/* LIST OF LESSONS */}
-                    {category.tutorials.map((item, index) => {
-                        return <LessonListItem category_id={category.id} handlerDelete={handlerLessonDelete} key={index} item={item} index={index} />
-                    })}
-
-                    <Link to={routeToLessonEdit(category.id)} className="button courseFormSubmit fill">Add Lessons</Link>
-                </div>
-            </form>
-            <Dialog
-                open={open}
-                fullWidth={true}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">{"Are you sure?"}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Delete "{category.title}"?
-                </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>
-                        Disagree
-                </Button>
-                    <Button onClick={() => handlerDelete(category.id)} color="secondary">
-                        Delete
-                </Button>
-                </DialogActions>
-            </Dialog>
+  return (
+    <div className="editCourseForm">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="actionButtons">
+          <a className="alertColor" onClick={handleClickOpen}>
+            DELETE COURSE
+          </a>
+          {!inProcess ? (
+            <button disabled={inProcess} type="submit">
+              SAVE CHANGES
+            </button>
+          ) : (
+            <button disabled>LOADING...</button>
+          )}
         </div>
-    )
-}
+        <h3 className="courseFormName">Edit course</h3>
+        <div className="w-660">
+          <div className="mb-30 row">
+            <div className="placeholder_img">
+              <label htmlFor="course_img">
+                {targetImage && !deleteFlag ? (
+                  <img
+                    src={config.REACT_APP_ROOT + targetImage}
+                    className="w-full"
+                  />
+                ) : (
+                  <p className="course_image_name">{uploadedFileName}</p>
+                )}
+              </label>
+            </div>
+            <div>
+              <input
+                onChange={fileChangeHandler}
+                type="file"
+                name="course_img"
+                id="course_img"
+                className="avatar_input"
+              />
+              {/* <p className='upoload_descr'>Add image</p> */}
+              <label htmlFor="course_img" className="ava_change_label">
+                Change
+              </label>
+              <button className="ava_delete_label" onClick={imageDeleteHandler}>
+                Delete
+              </button>
+            </div>
+          </div>
+          <p className="input_group_label">Main info about course</p>
+          <input
+            className="input_dark w-full"
+            name="title"
+            ref={register({ required: true, max: 100 })}
+            defaultValue={category.title}
+            placeholder="Tittle (max 100 characters)"
+          />
+          {errors && errors.title && (
+            <span className="error">Please, add valid title</span>
+          )}
+
+          <input
+            className="input_dark w-full"
+            name="description"
+            ref={register({ required: true, max: 1000 })}
+            defaultValue={category.description}
+            placeholder="Description (max 1 000 characters)"
+          />
+          {errors && errors.description && (
+            <span className="error">Please, add valid description</span>
+          )}
+
+          {reqErrors &&
+            reqErrors.title &&
+            reqErrors.title.length > 0 &&
+            reqErrors.title.map((error) => (
+              <span className="error">
+                {error}
+                <br />
+              </span>
+            ))}
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                color="primary"
+              />
+            }
+            label="Remote learning course"
+          />
+          <p className="input_group_label">List of the Module:</p>
+
+          {/* LIST OF LESSONS */}
+          {category.tutorials.map((item, index) => {
+            return (
+              <LessonListItem
+                category_id={category.id}
+                handlerDelete={handlerLessonDelete}
+                key={index}
+                item={item}
+                index={index}
+              />
+            );
+          })}
+
+          <Link
+            to={routeToLessonEdit(category.id)}
+            className="button courseFormSubmit fill"
+          >
+            Add Lessons
+          </Link>
+        </div>
+      </form>
+      <Dialog
+        open={open}
+        fullWidth={true}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Are you sure?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Delete "{category.title}"?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={() => handlerDelete(category.id)} color="secondary">
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
 
 const mapStateToProps = ({ admin }) => {
-    const data = {
-        reqErrors: admin.errors,
-        inProcess: admin.inProcess
-    }
-    return data
-}
+  const data = {
+    reqErrors: admin.errors,
+    inProcess: admin.inProcess,
+  };
+  return data;
+};
 
-EditCourseForm.propTypes = {
-}
+EditCourseForm.propTypes = {};
 
-export default connect(mapStateToProps, null)(EditCourseForm)
+export default connect(mapStateToProps, null)(EditCourseForm);
